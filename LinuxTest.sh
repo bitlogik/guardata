9c391eae1084b35e35498ce43d745b34fa1e2f4a
Antoine FERRON [BitLogiK] <antoine.ferron@bitlogik.fr>
2020-07-24 11:17:55 +0000

    Add GitLab CI/CD
    Update .gitlab-ci.yml
    
    Workaround TZ input blocking
    
    make sure fuse is installed
    
    mulitline pytest command
    
    Test GitLab CI with a script
    
    CRLF to CR for the bash script
    
    Update script
    
    Finish Linux test
    
    Add windows test
    
    fix win image
    
    try Windows shared runners
    
    win test 01
    
    Update win
    
    win update
    
    Accept auto for choco install
    
    fix win cd
    
    update win
    
    win update
    
    try python work in win
    
    Make python in win finally work
    
    try sed version for win
    
    fix win build
    
    Win Test
    
    win install SDK for RC
    
    forget force yes for SDK install
    
    VS BuildTools for RC
    
    Try add VS VC tools
    
    now trying VC workload desktop
    
    try add optional pkg for VS wl desktop
    
    one more try for VS
    
    one more VC tools try
    
    one more VC pkg try
    
    yet another test for VC
    
    one more VC time
    
    New win test
    
    Try specifying rc path
    
    Add C++ desktop
    
    windows try with many path for bin & headers
    
    headers in INCLUDE env var
    
    Use VS Dev Command Prompt
    
    Just call the BAT script
    
    try yaml syntax
    
    Import env from VCvars batch
    
    Fix PS script call
    
    Change choco NSIS pkg
    
    Add NSIS path
    
    Adjust artifact path
    
    Add Linux test & prod wheel
    
    Try CICD Python 37
    
    Update GitLab CICD : add Win tests
    
    many fixes in Win tests
    
    Fix win test pipeline : get winfsptest
    
    one more fix win tests : get winfsptest again
    
    update gitlab win tests
    
    try win tests wheel dir
    
    try fix win tests : wheel list
    
    win tests ; wheel from dist
    
    Fix win tests build
    
    try fixing win tests
    
    gitlab win tests : whl name
    
    final fix for wheel file name

---
+++ LinuxTest.sh
@@ -0,0 +1,15 @@
+
+set -eux
+
+pip3 install -U testresources psutil
+pip3 install -r pre-requirements.txt
+python3 setup.py bdist_wheel
+pip3 install $(ls dist/parsec_cloud-*.whl)[all]
+
+mkdir -p empty
+cp -R tests empty
+cp setup.cfg empty
+cd empty
+
+/home/testuser/.local/bin/py.test --log-level=DEBUG --durations=10 -v tests --runmountpoint --runslow -n auto --max-worker-restart=0 -x
+/home/testuser/.local/bin/py.test --log-level=DEBUG --durations=10 -v tests/backend tests/test_cli.py --postgresql --runslow -n auto --max-worker-restart=0
