f8ca25cbc0870b6c150233c2f6c22ba95ceb958f
Antoine FERRON <antoine.ferron@bitlogik.fr>
2020-08-15 01:21:26 +0200

    Fix tests

--- tests/scripts/run_testenv.py
+++ tests/scripts/run_testenv.py
@@ -130,6 +130,7 @@ async def restart_local_backend(administration_token, backend_port):
     command = (
         f"{sys.executable} -Wignore -m parsec.cli backend run -b MOCKED --db MOCKED "
         f"-P {backend_port} --administration-token {administration_token} --backend-addr parsec://localhost:{backend_port}?no_ssl=true"
+        f" --email-sender aaa@mydomain.com"
     )
 
     # Trio does not support subprocess in windows yet
