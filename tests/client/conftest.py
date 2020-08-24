6fb9af850ba1501c203e094e8641968bb77990ce
Antoine FERRON <antoine.ferron@bitlogik.fr>
2020-08-24 17:25:10 +0200

    fix remaining import

--- tests/client/conftest.py
+++ tests/client/conftest.py
@@ -12,7 +12,6 @@ from guardata.client.backend_connection import (
 from guardata.client.remote_devices_manager import RemoteDevicesManager
 from guardata.client.fs import UserFS
 from parsec.core.logged_core import get_pattern_filter
-from guardata.client.logged_client import DEFAULT_PATTERN_FILTER
 
 
 @pytest.fixture
