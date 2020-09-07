a4bac570d03664ecd30302ad6a2ec570a28be4e0
Antoine FERRON <antoine.ferron@bitlogik.fr>
2020-09-07 20:05:48 +0200

    Better Python calls in pipelines

--- tests/LinuxTest.sh
+++ tests/LinuxTest.sh
@@ -13,5 +13,5 @@ cp -R backendService dir4tests
 cp setup.cfg dir4tests
 cd dir4tests
 
-/home/testuser/.local/bin/py.test --log-level=DEBUG --durations=10 -v tests --runmountpoint --runslow -n auto --max-worker-restart=0 -x
-/home/testuser/.local/bin/py.test --log-level=DEBUG --durations=10 -v tests/backend tests/test_cli.py --postgresql --runslow -n auto --max-worker-restart=0
+python3 -m pytest --log-level=DEBUG --durations=10 -v tests --runmountpoint --runslow -n auto --max-worker-restart=0 -x
+python3 -m pytest --log-level=DEBUG --durations=10 -v tests/backend tests/test_cli.py --postgresql --runslow -n auto --max-worker-restart=0 -x
