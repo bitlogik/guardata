60709fa89f09376d2c762e13e0bcc85c0e4f020b
Antoine FERRON <antoine.ferron@bitlogik.fr>
2020-10-13 19:27:37 +0200

    Add log in MacOS

--- packaging/MacOSX/build_mac.sh
+++ packaging/MacOSX/build_mac.sh
@@ -8,8 +8,8 @@ python3 -m pip install 'pyinstaller==4.0'
 python3 -m pip install -U certifi
 cd packaging/MacOSX
 MACRUNfile='../../guardata/client/cli/run_mac.py'
-echo 'import sys' > $MACRUNfile
-echo 'from guardata.cli import cli' >> $MACRUNfile
-echo 'sys.argv = [sys.argv[0], "client", "gui", *sys.argv[1:]]' >> $MACRUNfile
+echo 'import sys, pathlib ; from guardata.cli import cli' > $MACRUNfile
+echo 'pathlib.Path("$HOME/.config/guardata").mkdir(parents=True, exist_ok=True)' >> $MACRUNfile
+echo 'sys.argv = [sys.argv[0],"client","gui","--log-level=INFO","--log-file=$HOME/.config/guardata/guardata-client.log"]+sys.argv[1:]' >> $MACRUNfile
 echo 'cli.main()' >> $MACRUNfile
 pyinstaller guardata.spec
