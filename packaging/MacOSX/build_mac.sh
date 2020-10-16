b7a9b915b4a3403b7449cde6ddd3b50558eb78b5
Antoine FERRON <antoine.ferron@bitlogik.fr>
2020-10-16 12:32:07 +0200

    Try fix URL scheme Mac

--- packaging/MacOSX/build_mac.sh
+++ packaging/MacOSX/build_mac.sh
@@ -11,6 +11,6 @@ MACRUNfile='../../guardata/client/cli/run_mac.py'
 echo 'import sys, pathlib ; from guardata.cli import cli' > $MACRUNfile
 echo 'config_folder = pathlib.Path(pathlib.Path.home()/".config/guardata")' >> $MACRUNfile
 echo 'config_folder.mkdir(parents=True, exist_ok=True)' >> $MACRUNfile
-echo 'sys.argv = [sys.argv[0],"client","gui","--log-level=INFO",f"--log-file={config_folder}/guardata-client.log"]+sys.argv[1:]' >> $MACRUNfile
+echo 'sys.argv = [sys.argv[0],"client","gui","--log-level=INFO",f"--log-file={config_folder}/guardata-client.log",*sys.argv[1:]]' >> $MACRUNfile
 echo 'cli.main()' >> $MACRUNfile
 pyinstaller guardata.spec
