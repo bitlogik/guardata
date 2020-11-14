09ac20d50dda6b4cbc62133079f39c4ab1856c73
Antoine FERRON <antoine.ferron@bitlogik.fr>
2020-11-15 00:57:51 +0100

    Improve Mac build

--- packaging/MacOSX/build_mac.sh
+++ packaging/MacOSX/build_mac.sh
@@ -3,14 +3,14 @@
 
 cd ../..
 python3 setup.py bdist_wheel
-python3 -m pip install .
-python3 -m pip install 'pyinstaller==4.0'
-python3 -m pip install -U certifi
+python3 -m pip --use-feature=2020-resolver install .
+python3 -m pip --use-feature=2020-resolver install 'pyinstaller==4.0'
+python3 -m pip --use-feature=2020-resolver install -U certifi
 cd packaging/MacOSX
 MACRUNfile='../../guardata/client/cli/run_mac.py'
 echo 'import sys, pathlib ; from guardata.cli import cli' > $MACRUNfile
 echo 'config_folder = pathlib.Path(pathlib.Path.home()/".config/guardata")' >> $MACRUNfile
 echo 'config_folder.mkdir(parents=True, exist_ok=True)' >> $MACRUNfile
-echo 'sys.argv = [sys.argv[0],"client","gui","--log-level=INFO",f"--log-file={config_folder}/guardata-client.log",*sys.argv[1:]]' >> $MACRUNfile
-echo 'cli.main()' >> $MACRUNfile
+echo 'argsv = ["client","gui","--log-level=INFO",f"--log-file={config_folder}/guardata-client.log",*sys.argv[1:]]' >> $MACRUNfile
+echo 'cli(argsv)' >> $MACRUNfile
 pyinstaller guardata.spec
