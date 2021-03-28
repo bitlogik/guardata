3d5e7f30d445e1466f87129e87ae8c6f4f7e36c1
Antoine FERRON [BitLogiK] <antoine.ferron@bitlogik.fr>
2021-03-28 23:54:36 +0200

    Update ws lib

--- setup.py
+++ setup.py
@@ -250,7 +250,7 @@ requirements = [
     "attrs==19.2.0",
     "click==7.0",
     "msgpack==0.6.2",
-    "wsproto==0.15.0",
+    "wsproto==1.0.0",
     "toastedmarshmallow==0.2.6",
     "pendulum==2.1.2",
     "PyNaCl==1.4.0",
